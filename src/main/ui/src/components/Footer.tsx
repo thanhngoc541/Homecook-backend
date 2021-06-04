2c54caf6b0a68686310d39225760bc9aba20f8f3
dangtrannam <dangtrannam2001@gmail.com>
2021-06-04 07:45:28 +0700

    Libs added & Initialize components

---
+++ src/main/ui/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Footer(props){
+
+}
+
+export default Footer
---
+++ src/main/ui/src/components/Main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Switch, Route, Redirect, withRouter } from "react-router-dom"
+import { TransitionGroup, CSSTransition } from "react-transition-group"
+import Header from './Header'
+import Footer from './Footer'
+
+function Main(props){
+    return (
+        <div>
+            <Header />
+            <TransitionGroup>
+                <CSSTransition>
+                    <Switch>
+                        <Route/>
+                        <Redirect to ='/home' />
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
+            <Footer />
+        </div>
+    )
+}
+
+export default withRouter(Main)
