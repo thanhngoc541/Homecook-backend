b3fac0457d467f6706cf9aeb27825e0cbe29a08c
HuyNguyen <ntghuy@gmail.com>
2021-07-12 10:34:43 +0700

    dang fix cor

--- src/main/java/Utils/CORSFilter.java
+++ src/main/java/Utils/CORSFilter.java
@@ -45,9 +45,12 @@ public class CORSFilter implements Filter {
         System.out.println("CORSFilter HTTP Request: " + request.getMethod());
 
         // Authorize (allow) all domains to consume the content
+        //Access-Control-Allow-Headers
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Expose-Headers", "Location");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
+
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
 
 
