ebe4e51ff63d166e8607a9acb3b46c5de3320a55
HuyNguyen <ntghuy@gmail.com>
2021-07-16 10:44:39 +0700

    before merge

--- src/main/java/services/OrderServices.java
+++ src/main/java/services/OrderServices.java
@@ -134,9 +134,11 @@ public class OrderServices {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/updateStatus/{id}/{status}")
-    public Response updateOrder02(String data) throws SQLException {
-        Order order= gson.fromJson(data, Order.class);
-        boolean result= service.changeOrderStatus(order.getOrderID(), order.getStatus());
+    public Response updateOrder02(@PathParam("id") String orderID, @PathParam("status") String status) throws SQLException {
+//        Order order= gson.fromJson(data, Order.class);
+//        Order order= new Order();
+//        int stat=order.getStatusID(status);
+        boolean result= service.changeOrderStatus(orderID, status);
         return result ? Response.ok().build() : Response.notModified().build();
     }
     @DELETE
