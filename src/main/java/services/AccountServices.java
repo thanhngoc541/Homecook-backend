5dc272e3275838d69bd85f6f4a179d2695dcef63
HuyNguyen <ntghuy@gmail.com>
2021-07-21 09:42:15 +0700

    pull

--- src/main/java/services/AccountServices.java
+++ src/main/java/services/AccountServices.java
@@ -60,8 +60,7 @@ public class AccountServices {
         String result= gson.toJson(accounts);
         return result;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     @GET
     @Path("/count/{role}/{username}")
     public String getTotalSearchedAccount(@PathParam("role") String role ,@PathParam("username") String username) {
@@ -70,10 +69,7 @@ public class AccountServices {
         String result= gson.toJson(total);
         return result;
     }
-=======
->>>>>>> b9a7278 (pull)
-=======
->>>>>>> bbb74ed (pull)
+
     @GET
     @Path("/username/{username}")
     @Produces(MediaType.APPLICATION_JSON)
